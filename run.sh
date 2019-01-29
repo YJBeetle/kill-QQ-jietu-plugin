b77105f87568cc9c8b7ac106dbbaf348d54f7675
YJBeetle <yjbeetle@gmail.com>
2019-01-29 19:04:41 +0800

    修复错误 取消备份文件的运行权限

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd '/Applications/QQ.app/Contents/Library/LoginItems/QQ jietu plugin.app/Contents/MacOS/'
 mv QQ\ jietu\ plugin QQ\ jietu\ plugin.bak
+chmod 000 QQ\ jietu\ plugin.bak
 echo '#!/bin/bash' > QQ\ jietu\ plugin
-echo 'sleep 10' > QQ\ jietu\ plugin
+echo 'sleep 999999' >> QQ\ jietu\ plugin
 chmod 555 QQ\ jietu\ plugin
 chown root:wheel QQ\ jietu\ plugin
